ad3a7887173d2b9eee05dcf4cea16ac527fed2ae
isavrodrigues <isavrodriguess@gmail.com>
2024-01-29 08:34:26 -0300

    colisao entre circulos

--- game_screen.py
+++ game_screen.py
@@ -1,6 +1,18 @@
 import pygame
 from config import FPS, WIDTH, HEIGHT, BLACK
 from assets import carrega_arquivos
+import math
+
+def colisao_entre_circulos(x1, y1, raio1, x2, y2, raio2):
+    # Calcula a distância entre os centros das circunferências
+    distancia_centros = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    # Verifica se a distância é menor ou igual à soma dos raios das circunferências
+    if distancia_centros <= raio1 + raio2:
+        return True
+    else:
+        return False
+    
 
 def game_screen(window):
     # Variável para o ajuste de velocidade
